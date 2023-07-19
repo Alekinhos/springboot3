913dfc55779b08c1759e52f00667e5d9c6aab7ae
Alex Silva <alex.silva.05@outlook.com>
2023-07-19 14:23:42 -0300

    configurando

--- src/main/java/com/empress/springboot/SpringbootApplication.java
+++ src/main/java/com/empress/springboot/SpringbootApplication.java
@@ -1,7 +1,16 @@
 package com.empress.springboot;
 
+import jakarta.servlet.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.ServerProperties;
+import org.springframework.boot.web.servlet.ServletContextInitializer;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import javax.faces.webapp.FacesServlet;
+import java.io.IOException;
 
 @SpringBootApplication
 public class SpringbootApplication {
@@ -10,4 +19,45 @@ public class SpringbootApplication {
 		SpringApplication.run(SpringbootApplication.class, args);
 	}
 
+	@Bean
+	public ServletRegistrationBean facesServletRegistration() {
+		Servlet servlet = new Servlet() {
+			@Override
+			public void init(ServletConfig config) throws ServletException {
+			}
+			@Override
+			public ServletConfig getServletConfig() {
+				return null;
+			}
+			@Override
+			public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+
+			}
+			@Override
+			public String getServletInfo() {
+				return null;
+			}
+			@Override
+			public void destroy() {
+
+			}
+		};
+		ServletRegistrationBean registration = new ServletRegistrationBean(servlet, "*.xhtml");
+		registration.setLoadOnStartup(1);
+		registration.addUrlMappings("*.jr");
+		return registration;
+	}
+
+	@Bean
+	public ServletContextInitializer servletContextInitializer(){
+		return servletContext -> {
+			servletContext.setInitParameter("com.sun.faces.forceLoadConfiguration", Boolean.TRUE.toString());
+			servletContext.setInitParameter("primefaces.THEME", "redmond");
+		};
+	}
+
+	@Bean
+	public RestTemplate restTemplate() { // biblioteca para comunicação rest
+		return new RestTemplate();
+	}
 }
